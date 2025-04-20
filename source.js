a7aa0b331312c4d5e50639f2f200654f13c35429
BlackFoxR5 <anshumayborah99@gmail.com>
2025-04-20 20:51:41 +0530

    boxSizing

--- source.js
+++ source.js
@@ -1,20 +1,22 @@
 function gridNumber() {
     let n = prompt ("Enter grid number");
     n = Number(n);
-    if (n < 10 || n > 100) {
-        return "Enter a value between 10 and 100";
+    if (n < 1|| n > 50) {
+        return "Enter a value between 1 and 50";
     } else return n;
 }
 
-let container = document.querySelector(".container");
-let body = document.querySelector("body");
-
+const container = document.querySelector(".container");
 let n = gridNumber();
-for (i = 0; i < n * n; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("gridsquares");
-    container.appendChild(cell);
-}
 
+let squareSize = 500 / n;
 
-
+for(let i = 1; i <= n * n; i ++) {
+    const gridsquares = document.createElement("div");
+    gridsquares.classList.add("gridsquares");
+    gridsquares.style.width = `${squareSize}px`;
+    gridsquares.style.height = `${squareSize}px`;
+    gridsquares.style.flexBasis = `${squareSize}px`;
+    gridsquares.style.boxSizing = "border-box";
+    container.appendChild(gridsquares);
+};
