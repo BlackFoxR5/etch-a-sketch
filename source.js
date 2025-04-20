77e01f70dc5d6e843d582c6cfafbd7cc8b50a054
BlackFoxR5 <anshumayborah99@gmail.com>
2025-04-20 23:40:19 +0530

    hover effect added

--- source.js
+++ source.js
@@ -1,22 +1,48 @@
-function gridNumber() {
-    let n = prompt ("Enter grid number");
-    n = Number(n);
-    if (n < 1|| n > 50) {
-        return "Enter a value between 1 and 50";
-    } else return n;
+const container = document.querySelector(".container");
+const gridsize = document.querySelector(".size");
+const reset = document.querySelector(".reset");
+
+let n = 10;
+
+function createGrid(n) {
+    container.textContent = "";
+
+    let squareSize = 500 / n;
+    
+    //creates grid
+    for(let i = 1; i <= n * n; i ++) {
+        const gridsquares = document.createElement("div");
+        gridsquares.classList.add("gridsquares");
+        gridsquares.style.width = `${squareSize}px`;
+        gridsquares.style.height = `${squareSize}px`;
+        gridsquares.style.flexBasis = `${squareSize}px`;
+        gridsquares.style.boxSizing = "border-box";
+        container.appendChild(gridsquares);
+    };
+
+    //hover effect
+    document.querySelectorAll(".gridsquares").forEach(item => {
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "hotpink";
+        });
+    });    
 }
 
-const container = document.querySelector(".container");
-let n = gridNumber();
+createGrid(n); //default grid of size 10
 
-let squareSize = 500 / n;
+gridsize.addEventListener("click", () => {
+    let size = Number(prompt ("Enter grid number"));
+    if(!isNaN(size) && size > 0 && size < 100) {
+        n = size;
+        createGrid(n);
+    } else {
+        alert("Enter digit between 0 and 50")
+    }
+    console.log(n);
+});
 
-for(let i = 1; i <= n * n; i ++) {
-    const gridsquares = document.createElement("div");
-    gridsquares.classList.add("gridsquares");
-    gridsquares.style.width = `${squareSize}px`;
-    gridsquares.style.height = `${squareSize}px`;
-    gridsquares.style.flexBasis = `${squareSize}px`;
-    gridsquares.style.boxSizing = "border-box";
-    container.appendChild(gridsquares);
-};
+reset.addEventListener("click", () => {
+    document.querySelectorAll(".gridsquares").forEach(item => {
+        item.style.backgroundColor = "lightblue";
+    }); 
+});
